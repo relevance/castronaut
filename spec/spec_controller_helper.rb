2c4aed336307d8828ba8a8eed9ca008beec3ea5b
Chad Humphries <chad@spicycode.com>
2008-09-03 17:09:33 -0400

    Clean up requires so it gets the vendored sinatra

--- spec/spec_controller_helper.rb
+++ spec/spec_controller_helper.rb
@@ -1,8 +1,9 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '../vendor/sinatra/lib')
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../vendor/sinatra/lib')
 
 require 'sinatra'
+require 'sinatra/test/unit'
 
 Sinatra::Application.default_options.merge!(
   :env => :test,
@@ -12,4 +13,3 @@ Sinatra::Application.default_options.merge!(
 )
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'app', 'controllers', 'application'))
-require 'sinatra/test/unit'
