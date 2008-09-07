9893e47a54a868bef882de15b58809c3004187dd
Chad Humphries <chad@spicycode.com>
2008-09-07 15:41:06 -0400

    Adding example of ar test

--- spec/castronaut/models/login_ticket_spec.rb
+++ spec/castronaut/models/login_ticket_spec.rb
@@ -2,10 +2,11 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'spec_hel
 
 describe Castronaut::Models::LoginTicket do
 
-  describe "generating a new one" do
-    
-    it "must be given a client host" do
-      Castronaut::Models::LoginTicket.generate_from("client host").should_not be_nil
+  describe "generating from a client hostname" do
+   
+    it "initializes a new Login Ticket with given client hostname" do
+      Castronaut::Models::LoginTicket.expects(:new).with(:client_host => 'ch').returns(stub_everything)
+      Castronaut::Models::LoginTicket.generate_from('ch')
     end
     
   end
