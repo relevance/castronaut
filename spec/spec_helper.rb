3bdd86f20ead178b673a8a3e453e1f1af711e16d
Aaron J. Bedra <aaron@thinkrelevance.com>
2008-09-08 19:46:32 -0400

    modifying spec helper to include migration for restful auth adapter test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,27 @@ require File.join(File.dirname(__FILE__), '..', 'castronaut')
 
 Castronaut.config = Castronaut::Configuration.new(File.join(File.dirname(__FILE__), '..', 'castronaut.example.yml'))
 
+class CreateUsers < ActiveRecord::Migration
+  old_connection = ActiveRecord::Base.connection
+  ActiveRecord::Base.connection = Castronaut::Adapters::RestfulAuthentication::User.connection
+
+  create_table "users", :force => true do |t|
+    t.column :login,                     :string, :limit => 40
+    t.column :name,                      :string, :limit => 100, :default => '', :null => true
+    t.column :email,                     :string, :limit => 100
+    t.column :crypted_password,          :string, :limit => 40
+    t.column :salt,                      :string, :limit => 40
+    t.column :created_at,                :datetime
+    t.column :updated_at,                :datetime
+    t.column :remember_token,            :string, :limit => 40
+    t.column :remember_token_expires_at, :datetime
+    t.string :first_name, :last_name
+  end
+  add_index :users, :login, :unique => true
+
+  ActiveRecord::Base.connection = old_connection
+end
+
 Spec::Runner.configure do |config|
   config.mock_with :mocha
 end
