c2f34c2fe99185f4073cfe6cd8060e11199740a8
Chad Humphries <chad@spicycode.com>
2008-09-08 17:48:25 -0400

    Making tests use the example yml

--- spec/castronaut_spec.rb
+++ spec/castronaut_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe Castronaut do
   
   after(:all) do
-    Castronaut.config = Castronaut::Configuration.new(File.join(File.dirname(__FILE__), '..', 'castronaut.yml'))
+    Castronaut.config = Castronaut::Configuration.new(File.join(File.dirname(__FILE__), '..', 'castronaut.example.yml'))
   end
   
   it "exposes the config at Castronaut.config" do
