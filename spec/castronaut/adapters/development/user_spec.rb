e4c0f4a41017be3ab2a3b6ceb37081e027665578
Aaron J. Bedra <aaron@thinkrelevance.com>
2008-10-20 23:11:01 -0400

    adding tests for development user adapter

---
+++ spec/castronaut/adapters/development/user_spec.rb
@@ -0,0 +1,55 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'spec_helper'))
+
+describe Castronaut::Adapters::Development::User do
+
+  describe "find by login" do
+    
+    it "attempts to the find the first user with the given login" do
+      Castronaut::Adapters::Development::User.should_receive(:find_by_login).and_return(nil)
+      Castronaut::Adapters::Development::User.find_by_login('bob')
+    end
+    
+    it "returns nil if no user is found with the given login" do
+      Castronaut::Adapters::Development::User.stub!(:find).and_return(nil)
+      Castronaut::Adapters::Development::User.find_by_login('bob').should be_nil
+    end
+
+    it "returns the user found with the given login" do
+      Castronaut::Adapters::Development::User.stub!(:find_by_login).and_return(user = stub('user'))
+      Castronaut::Adapters::Development::User.find_by_login('bob').should == user
+    end
+    
+  end
+  
+  describe "authenticate" do
+    
+    it "attempts to find the user by the username / login" do
+      Castronaut::Adapters::Development::User.should_receive(:find_by_login).with('bob').and_return(nil)
+      Castronaut::Adapters::Development::User.authenticate('bob', '1234')
+    end
+    
+    describe "when the user is not found" do
+      
+      it "returns a Castronaut::AuthenticationResult object with the unable to authenticate user message" do
+        Castronaut::Adapters::Development::User.stub!(:find_by_login).and_return(nil)
+        Castronaut::Adapters::Development::User.authenticate('bob', '1234').error_message.should == "Unable to authenticate the username bob"
+      end
+      
+    end
+    
+    describe "when the user is found" do
+      
+      describe "when the credentials are invalid" do
+        
+        it "returns a Castronaut::AuthenticationResult object with the unable to authenticate user message" do
+          Castronaut::Adapters::Development::User.stub!(:find_by_login).and_return(stub_everything(:crypted_password => "a", :salt => "b"))
+          Castronaut::Adapters::Development::User.authenticate('bob', '1234').error_message.should == "Unable to authenticate the username bob"
+        end
+        
+      end
+         
+    end
+    
+  end
+
+end
