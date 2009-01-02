3843a60020c33d13e5208ba9bb1d56acbe9c930b
Aaron Bedra <aaron@thinkrelevance.com>
2009-01-02 10:44:21 -0500

    fixed outdated activerecord calls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,7 @@ require File.join(File.dirname(__FILE__), 'spec_rails_mocks')
 Castronaut.config = Castronaut::Configuration.load(File.join(File.dirname(__FILE__), '..', 'config', 'castronaut.example.yml'))
 
 class CreateUsers < ActiveRecord::Migration
-  old_connection = ActiveRecord::Base.connection
-  ActiveRecord::Base.connection = Castronaut::Adapters::RestfulAuthentication::User.connection
-
-  create_table "users", :force => true do |t|
+  Castronaut::Adapters::RestfulAuthentication::User.connection.create_table "users", :force => true do |t|
     t.column :login,                     :string, :limit => 40
     t.column :name,                      :string, :limit => 100, :default => '', :null => true
     t.column :email,                     :string, :limit => 100
@@ -25,23 +22,16 @@ class CreateUsers < ActiveRecord::Migration
     t.column :remember_token_expires_at, :datetime
     t.string :first_name, :last_name
   end
-  add_index :users, :login, :unique => true
-
-  ActiveRecord::Base.connection = old_connection
+  Castronaut::Adapters::RestfulAuthentication::User.connection.add_index :users, :login, :unique => true
 end
 
 class CreateUsers < ActiveRecord::Migration
-  old_connection = ActiveRecord::Base.connection
-  ActiveRecord::Base.connection = Castronaut::Adapters::Development::User.connection
-
-  create_table "users", :force => true do |t|
+  Castronaut::Adapters::Development::User.connection.create_table "users", :force => true do |t|
     t.column :login,                     :string, :limit => 40
     t.column :name,                      :string, :limit => 100, :default => '', :null => true
     t.column :password,                  :string
   end
-  add_index :users, :login, :unique => true
-
-  ActiveRecord::Base.connection = old_connection
+  Castronaut::Adapters::Development::User.connection.add_index :users, :login, :unique => true
 end
 
 Spec::Runner.configure do |config|
